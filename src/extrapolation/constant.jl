27a6f558dce9854c02f92cc90c07542ff498bef4
Tomas Lycken <tomas.lycken@gmail.com>
2015-07-25 02:26:06 +0200

    Update constant extrap @nref usage

--- src/extrapolation/constant.jl
+++ src/extrapolation/constant.jl
@@ -7,15 +7,9 @@ ConstantExtrapolation{T,ITP,IT,GT}(::Type{T}, N, itp::ITP, ::Type{IT}, ::Type{GT
 extrapolate{T,N,IT,GT}(itp::AbstractInterpolation{T,N,IT,GT}, ::Type{Flat}) =
     ConstantExtrapolation(T,N,itp,IT,GT)
 
-function extrap_prep{T,ITP,IT}(etp::Type{ConstantExtrapolation{T,1,ITP,IT,OnGrid}}, x)
-    :(x = clamp(x, 1, size(etp,1)))
-end
-function extrap_prep{T,ITP,IT}(etp::Type{ConstantExtrapolation{T,1,ITP,IT,OnCell}}, x)
-    :(x = clamp(x, .5, size(etp,1)+.5))
-end
 function extrap_prep{T,N,ITP,IT}(etp::Type{ConstantExtrapolation{T,N,ITP,IT,OnGrid}}, xs...)
-    :(@nexprs $N d->(xs[d] = clamp(xs[d], 1, size(etp,d))))
+    :(@nexprs $N d->(x_d = clamp(xs[d], one($xs[d]), size(etp,d))))
 end
 function extrap_prep{T,N,ITP,IT}(etp::Type{ConstantExtrapolation{T,N,ITP,IT,OnCell}}, xs...)
-    :(@nexprs $N d->(xs[d] = clamp(xs[d], .5, size(etp,d)+.5)))
+    :(@nexprs $N d->(x_d = clamp(xs[d], 1//2, size(etp,d)+1//2)))
 end
--- src/extrapolation/periodic.jl
+++ src/extrapolation/periodic.jl
@@ -8,5 +8,5 @@ extrapolate{T,N,IT,GT}(itp::AbstractInterpolation{T,N,IT,GT}, ::Type{Periodic}) 
     PeriodicExtrapolation(T,N,itp,IT,GT)
 
 function extrap_prep{T,N,ITP,IT,GT}(etp::Type{PeriodicExtrapolation{T,N,ITP,IT,GT}}, xs...)
-    :(@nexprs $N d->(; x_d = mod(xs[d]-one($xs[d]), size(etp.itp,d)-one($xs[d]))+one($xs[d])))
+    :(@nexprs $N d->(x_d = mod(xs[d]-one($xs[d]), size(etp.itp,d)-one($xs[d]))+one($xs[d])))
 end
