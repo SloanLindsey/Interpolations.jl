fa3cb50a3777e66dfde58f85b16e92e6a48b03e9
Tomas Lycken <tomas.lycken@jayway.com>
2015-09-17 15:33:08 +0200

    Improve scaled testing

--- src/scaling/scaling.jl
+++ src/scaling/scaling.jl
@@ -5,13 +5,23 @@ end
 ScaledInterpolation{T,ITPT,IT,GT,RT}(::Type{T}, N, itp::ITPT, ::Type{IT}, ::Type{GT}, ranges::RT) =
     ScaledInterpolation{T,N,ITPT,IT,GT,RT}(itp, ranges)
 function scale{T,N,IT,GT}(itp::AbstractInterpolation{T,N,IT,GT}, ranges::Range...)
-    length(ranges) == N || error(string("Must scale $N-dimensional interpolation object with exactly $N ranges (you used $(length(ranges)))"))
+    length(ranges) == N || error("Must scale $N-dimensional interpolation object with exactly $N ranges (you used $(length(ranges)))")
+    for d in 1:N
+        iextract(IT,d) != NoInterp || ranges[d] == 1:size(itp,d) || error("NoInterp dimension $d must be scaled with unit range 1:$(size(itp,d))")
+    end
+
     ScaledInterpolation(T,N,itp,IT,GT,ranges)
 end
 
-@generated function getindex{T,N}(sitp::ScaledInterpolation{T,N}, xs...)
-    length(xs) == N || error(string("Must index into ScaledInterpolation{", N, "} with exactly ", N, " indices (you used ", length(xs), ")"))
-    :(getindex(sitp.itp, coordlookup(sitp.ranges, tuple(xs...))...))
+@generated function getindex{T,N,ITPT,IT<:DimSpec}(sitp::ScaledInterpolation{T,N,ITPT,IT}, xs...)
+    length(xs) == N || error("Must index into $N-dimensional scaled interpolation object with exactly $N indices (you used $(length(xs))")
+    if IT <: Tuple
+        # handle dimspecs
+        interp_indices = map(it -> IT.parameters[it] != NoInterp, 1:length(IT.parameters))
+        :(getindex(sitp.itp, map(coordlookup, $interp_indices, sitp.ranges, xs)...))
+    else
+        :(getindex(sitp.itp, coordlookup(sitp.ranges, tuple(xs...))...))
+    end
 end
 
 size(sitp::ScaledInterpolation, d) = size(sitp.itp, d)
@@ -19,12 +29,14 @@ size(sitp::ScaledInterpolation, d) = size(sitp.itp, d)
 coordlookup(r::FloatRange, x) = (r.divisor * x - r.start) ./ r.step + one(eltype(r))
 coordlookup(r::StepRange, x) = (x - r.start) ./ r.step + one(eltype(r))
 coordlookup(r::UnitRange, x) = x - r.start + one(eltype(r))
+coordlookup(i::Bool, r::Range, x) = i ? coordlookup(r, x) : x
 coordlookup{N}(r::NTuple{N}, x::NTuple{N}) = map(coordlookup,r,x)
+coordlookup{N}(i::NTuple{N}, r::NTuple{N}, x::NTuple{N}) = map(coordlookup, i, r, x)
 
 gradient{T,N}(sitp::ScaledInterpolation{T,N}, xs...) = gradient!(Array(T,N), sitp, xs...)
-@generated function gradient!{T,N}(g, sitp::ScaledInterpolation{T,N}, xs...)
+@generated function gradient!{T,N,ITPT,IT}(g, sitp::ScaledInterpolation{T,N,ITPT,IT}, xs...)
     ndims(g) == 1 || error(string("g must be a vector (but had ", ndims(g), " dimensions)"))
-    length(xs) == N || error(string("Must index into ScaledInterpolation{", N, "} with exactly ", N, " indices (you used ", length(xs), ")"))
+    length(xs) == count_interp_dims(IT, N) || error("Must index into ScaledInterpolation{$N} with exactly $N indices (you used $(length(xs))")
 
     quote
         length(g) == N || error(string("g must be a vector of length ", N, " (was ", length(g), ")"))
---
+++ test/scaling/withextrap.jl
@@ -0,0 +1,7 @@
+
+module ScalingWithExtrapTests
+
+using Interpolations
+
+
+end
